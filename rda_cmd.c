e2ec0a24805e5da62dd9ef1b1c6cc5ddbe174e4e
root <root@konev>
2018-04-03 19:49:43 +0300

    все сломал

--- rda_cmd.c
+++ rda_cmd.c
@@ -29,18 +29,17 @@ void send_pkt(struct packet *pkt)
 	if (pkt->pdl_pkt->data)
 		write_tty((char *)(pkt->pdl_pkt->data), pkt->pdl_pkt->cmd_header.data_size);
 	write_tty((char *)&(pkt->state), sizeof(pkt->state));
-
 }
 
-struct pdl_packet *add_cmd_hdr(u32 cmd_type, u32 cmd_data_addr, u32 cmd_data_size, u8 *data)
+struct pdl_packet *make_pdl_pkt(struct command_header *cmd_hdr, u8 *data)
 {
 	struct pdl_packet *pdl_pkt = malloc(sizeof(struct pdl_packet));
 
-	pdl_pkt->cmd_header.cmd_type = htole32(cmd_type);
+	pdl_pkt->cmd_header.cmd_type = htole32(cmd_hdr->cmd_type);
 
-	pdl_pkt->cmd_header.data_addr = htole32(cmd_data_addr);
+	pdl_pkt->cmd_header.data_addr = htole32(cmd_hdr->data_addr);
 
-	pdl_pkt->cmd_header.data_size = htole32(cmd_data_size);
+	pdl_pkt->cmd_header.data_size = htole32(cmd_hdr->data_size);
 
 	pdl_pkt->data = data;
 
@@ -68,14 +67,14 @@ void free_pkt_mem(struct packet *pkt)
 	free(pkt);
 }
 
-void send_cmd(u32 cmd_type, u32 cmd_data_addr, u32 cmd_data_size, u8 *data, u32 data_size)
+int send_cmd(struct command_header *cmd_hdr, u8 *data, u32 data_size)
 {
-	printf("\ncmd: %s(%d)\n", str_cmd(cmd_type), cmd_type);
+	printf("\ncmd: %s(%d)\n", str_cmd(cmd_hdr->cmd_type), cmd_hdr->cmd_type);
 
 	char rcv_buf[PDL_MAX_DATA_SIZE];
 
-	struct pdl_packet *pdl_pkt = add_cmd_hdr(cmd_type, cmd_data_addr, cmd_data_size, data);
-	struct packet *pkt = make_pkt(HOST_PACKET_TAG, HOST_PACKET_FLOWID, pdl_pkt, data_size);
+	struct pdl_packet *pdl_pkt = make_pdl_pkt(cmd_hdr, data);
+	struct packet *pkt = make_pkt(HOST_PACKET_TAG, HOST_PACKET_FLOWID, pdl_pkt, cmd_hdr->data_size);
 
 	send_pkt(pkt);
 
@@ -90,7 +89,7 @@ void send_cmd(u32 cmd_type, u32 cmd_data_addr, u32 cmd_data_size, u8 *data, u32 
 	printf("len: %d\n", len);
 
 	if (len <= 0)
-		return;
+		return -1;
 
 	hex_dump(rcv_buf, len);
 
@@ -98,18 +97,23 @@ void send_cmd(u32 cmd_type, u32 cmd_data_addr, u32 cmd_data_size, u8 *data, u32 
 	
 	if (pkt_hdr->flowid == FLOWID_ERROR) {
 		printf("flowid error\n");
-		return;
+		return -1; 
 	}
 	int rsp = le32toh(*(int *)(rcv_buf + sizeof(struct packet_header)));
 
 	printf("response: %s(%d)\n", str_rsp(rsp), rsp);
-
+	return 0;
 }
 
-#define send_cmd_only(cmd) send_cmd(cmd, 0, 0, NULL, 0)
-#define send_cmd_data(cmd, data, size) send_cmd(cmd, 0, 0, data, size)
-#define send_cmd_hdr(cmd, addr, data) send_cmd(cmd, addr, data, NULL, 0)
+int send_cmd_only(u32 cmd_type)
+{
+	struct command_header cmd_hdr;
 
+	memset(&cmd_hdr, 0, sizeof(struct command_header));
+	cmd_hdr.cmd_type = cmd_type;
+
+	return send_cmd(&cmd_hdr, NULL, 0);
+}
 
 int allowed_commands[] = {
 	//ERASE_FLASH,
@@ -145,59 +149,106 @@ int allowed_commands[] = {
 	//DOWNLOAD_FINISH,
 };
 
-#define PDL_PATH "pdl2.bin"
+#define PDL1_PATH "pdl1.bin"
+#define PDL2_PATH "pdl2.bin"
+
+#define PDL1_ADDR 0x00100100 //spl-uboot start addr
+#define PDL2_ADDR 0x80008000 
+
 #define CHUNK_SIZE 1024
+
+
+int upload_buf(u8 *buf, u32 data_size, u32 data_addr)
+{
+	int ret;
+	struct command_header cmd_hdr;
+
+	cmd_hdr.cmd_type = START_DATA;
+	cmd_hdr.data_addr = data_addr;
+	cmd_hdr.data_size = data_size;
+
+	ret = send_cmd(&cmd_hdr, NULL, 0);
+	if (ret)
+		return -1;
+
+	u32 total_send = 0;
+	while (total_send < data_size) {
+		
+		u32 chunk = CHUNK_SIZE;
+		if ((total_send + CHUNK_SIZE) > data_size)
+			chunk = data_size - total_send;
+
+		cmd_hdr.cmd_type = MID_DATA;
+		cmd_hdr.data_addr = 0;
+		cmd_hdr.data_size = chunk;
+
+		ret = send_cmd(&cmd_hdr, buf + total_send, chunk);
+		if (ret)
+			return -1;
+
+		total_send += chunk;
+	}
+
+	ret = send_cmd_only(END_DATA);
+	if (ret)
+		return -1;
+
+	return 0;
+}
+
+int upload_file(char *path, u32 data_addr)
+{
+	int fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		printf("can't open file: %s\n", path);
+		return -1;
+	}
+	struct stat stat_buf;
+	stat(path, &stat_buf);
+
+	char *buf = mmap(NULL, stat_buf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	if (buf == NULL) {
+		close(fd);
+		printf("can't mmap file: %s\n", path);
+		return -1;
+	}
+
+	upload_buf(buf, stat_buf.st_size, data_addr);
+
+	munmap(buf, stat_buf.st_size);
+
+	close(fd);
+	
+	return 0;
+}
 
 int main(void)
 {
 	if (open_tty() != 0)
 		return -1;
 
-    int fd = open(PDL_PATH, O_RDONLY);
-    if (fd < 0) {
-		printf("can't open pdl file\n");
-        return -1;
-	}
-
-	struct stat stat_buf;
-	stat(PDL_PATH, &stat_buf);
-
-	char *pdl_buf = mmap(NULL, stat_buf.st_size, PROT_READ, MAP_SHARED, fd, 0);
-	if (pdl_buf == NULL) {
-		close(fd);
-		printf("can't mmap pdl file\n");
+	if (send_cmd_only(CONNECT)) {
+		close_tty();
+		printf("can't connect to device\n");
 		return -1;
 	}
+	
 
+	//exec pdl1
+	upload_file(PDL1_PATH, PDL1_ADDR);
+	send_cmd_only(EXEC_DATA);
+
+	return 0;
+	sleep(3);
 
 	send_cmd_only(CONNECT);
 
-#if 1
-	//send_cmd_hdr(START_DATA, 0x00100100, stat_buf.st_size);
-	send_cmd_hdr(START_DATA, 0x00800800, stat_buf.st_size);
-
-	u32 total_send = 0;
-	while (total_send < stat_buf.st_size) {
-		
-		u32 chunk = CHUNK_SIZE;
-		if ((total_send + CHUNK_SIZE) > stat_buf.st_size)
-			chunk = stat_buf.st_size - total_send;
-	
-		send_cmd(MID_DATA, 0, chunk, pdl_buf + total_send, chunk);
-
-		total_send += chunk;
-	}
-
-	send_cmd_only(END_DATA);
-
+	//exec pdl2
+	upload_file(PDL2_PATH, PDL2_ADDR);
 	send_cmd_only(EXEC_DATA);
-#endif
+
 
 	//send_cmd_only(GET_VERSION);
-
-	munmap(pdl_buf, stat_buf.st_size);
-
-	close(fd);
 
 	close_tty();
 	return 0;
