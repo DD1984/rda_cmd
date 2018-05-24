91faf3cc301a01903cca27c7786e14774067157e
konev <konev@konev>
2018-05-24 13:55:48 +0300

    перенес pdls в отдельную папку

--- include/pdls.h
+++ include/pdls.h
@@ -1,8 +1,8 @@
 #ifndef __PDLS_H__
 #define __PDLS_H__
 
-#define PDL1_PATH "pdl1.bin"
-#define PDL2_PATH "pdl2.bin"
+#define PDL1_PATH "pdls/pdl1.bin"
+#define PDL2_PATH "pdls/pdl2.bin"
 
 #define PDL1_ADDR 0x00100100 //spl-uboot start addr
 #define PDL2_ADDR 0x80008000
